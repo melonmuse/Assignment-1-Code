f551c95f74567361e39d41ca62965a25d74072f6
Maima <asscracker8812@gmail.com>
2026-09-26 12:18:22 +0400

    completed ship, coordinate and battleship

---
+++ battleShip.java
@@ -0,0 +1,11 @@
+public class battleShip  extends ship{
+    private boolean armorUsed;
+
+    public battleShip(){
+        super("battleShip", 4);
+        armorUsed = false;
+    }
+
+    
+    
+}
